bf7a7f0b4d1ef5f17a5a6c2481a2307b07e7b9bb
Kumar-Saurabh-Tiwari <tiwarisaurabhkumar0@gmail.com>
2026-05-15 17:11:52 +0530

    update runtime configuration to nodejs for SSR and add function settings in Vercel config

--- api/ssr.ts
+++ api/ssr.ts
@@ -1,7 +1,7 @@
 import server from "../src/server";
 
 export const config = {
-  runtime: "edge",
+  runtime: "nodejs",
 };
 
 export default function handler(request: Request): Promise<Response> | Response {
